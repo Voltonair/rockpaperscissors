645157b35e335a9d7e1870da52c5b4dd65aaa75e
Voltonair <martinarmask@gmail.com>
2023-04-26 20:58:27 +0100

    Rock, paper and scissors exercise revisited

--- script.js
+++ script.js
@@ -1,167 +1,104 @@
-// DOM variables
-const displayName = document.querySelector("#display-name");
-const displayUser = document.querySelector("#display-user");
-const displayIA = document.querySelector("#display-computer");
-const selectBtn = document.querySelectorAll(".btn");
-const displayUserScore = document.querySelector("#user-score");
-const displayComputerScore = document.querySelector("#computer-score");
-const userForm = document.querySelector("#user-form");
-const textBox = document.querySelector("#text-box");
-const displayWinner = document.querySelector("#display-winner");
+let buttons = document.querySelectorAll('[data-selection]');
+let userSelection = document.querySelector('.user-selection');
+let computerSelection = document.querySelector('.computer-selection');
+let displayUserScore = document.querySelector('.user-score');
+let displayComputerScore = document.querySelector('.computer-score');
+let displayResultMessage = document.querySelector('.result');
 
-// Get audio variable
-const audio = document.querySelectorAll("[data-option]");
+let storeUserMessage = 'Your score: ';
+let storeComputerMessage = 'Computer score: ';
+let storeDefaultWinnerMessage = 'And the winner is... ';
 
-// Get score and player names
-let playerName = "";
-let scorePlayer = 0;
-let scoreIA = 0;
+let userScore = '';
+let computerScore = '';
 
+const disableMessage = buttons.length;
 
-// IA gets a random choice
-function getRandomNum() {
-    const choices = ["rock", "paper", "scissors"];
-    const randomNum = Math.floor(Math.random () * choices.length);
-    return choices[randomNum];
+function randomChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];
 }
 
-// Get player's name value
-function regInput (e) {
-    const value = e.value;
-    playerName = value;
+function setGame(player, computer) {
+    if (computer === 'rock') {
+        if (player === 'paper') {
+            userScore++;
+            displayUserScore.innerHTML = storeUserMessage + userScore;
+        } else if (player === 'scissors') {
+            computerScore++;
+            displayComputerScore.innerHTML = storeComputerMessage + computerScore;
+        }
+    }
+    if (computer === 'paper') {
+        if (player === 'scissors') {
+            userScore++;
+            displayUserScore.innerHTML = storeUserMessage + userScore;
+        } else if (player === 'rock') {
+            computerScore++;
+            displayComputerScore.innerHTML = storeComputerMessage + computerScore;
+        }
+    }
+    if (computer === 'scissors') {
+        if (player === 'rock') {
+            userScore++;
+            displayUserScore.innerHTML = storeUserMessage + userScore;
+        } else if (player === 'paper') {
+            computerScore++;
+            displayComputerScore.innerHTML = storeComputerMessage + computerScore;
+        }
+    }
+    if (computer === player) return;
 }
 
-// Get and store player's name
-userForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    displayName.innerHTML = "Hi " + playerName + ", " + "let's play!";
-    textBox.value = "";
-})
-
-// Play rounds
-function playGame(userChoice, computerChoice) {
-    if (userChoice === "rock") {
-        if (computerChoice === "scissors") {
-            scorePlayer++
-            displayWinner.innerHTML = "You win, rock beats scissors";
-        } else if (computerChoice === "paper") {
-            scoreIA++
-            displayWinner.innerHTML = "You lose, paper beats rock";
-        } else if (userChoice === computerChoice) {
-            displayWinner.innerHTML = "It's a tie!";
+function winnerIs() {
+    if (userScore >= 3) {
+        if (userScore > computerScore) {
+            displayResultMessage.textContent = storeDefaultWinnerMessage + 'You';
+            disableGame();
+            resetButton();
         }
-    }
-
-    if (userChoice === "paper") {
-        if (computerChoice === "rock") {
-            scorePlayer++
-            displayWinner.innerHTML = "You win, paper beats rock";
-        } else if (computerChoice === "scissors") {
-            scoreIA++
-            displayWinner.innerHTML = "You lose, scissors beats paper";
-        } else if (userChoice === computerChoice) {
-            displayWinner.innerHTML = "It's a tie!";
-        }
-    }
-
-    if (userChoice === "scissors") {
-        if (computerChoice === "paper") {
-            scorePlayer++
-            displayWinner.innerHTML = "You win, scissors beats paper";
-        } else if (computerChoice === "rock") {
-            scoreIA++
-            displayWinner.innerHTML = "You lose, rock beats scissors";
-        } else if (userChoice === computerChoice) {
-            displayWinner.innerHTML = "It's a tie!"; 
+    } else if (computerScore >= 3) {
+        if (computerScore > userScore) {
+            displayResultMessage.textContent = storeDefaultWinnerMessage + 'Computer';
+            disableGame();
+            resetButton();
         }
     }
 }
 
-// Get scores and make it sound.
-function getScore () {
-
-    // Get these 2 functions to set a time out
-    function getWinningSound () {
-        audio[0].play();
-    }
-
-    function getLosingSound () {
-        audio[1].play();
-    }
-    //
-
-    if (scorePlayer === 3) {
-        if (playerName !== "") {
-            displayWinner.innerHTML = playerName + " wins!!!";
-        } else {
-            displayWinner.innerHTML = "John Doe wins!!!";
-        }
-        setTimeout(getWinningSound, 700);
-    }
-
-    if (scoreIA === 3) {
-        displayWinner.innerHTML = "Computer wins, you lose!";
-        setTimeout(getLosingSound, 700);
+function disableGame() {
+    for (let i = 0; i < disableMessage; i++) {
+        buttons[i].disabled = true;
     }
 }
 
-// Convert nodelist into array for the sake of cleaning.
-const userSelection = Array.from(selectBtn);
-
-// For each button you click...
-userSelection.forEach(button => {
-    button.addEventListener("click", () => {
-        const computerChoice = getRandomNum();
-        const userChoice = button.dataset.select;
-
-        playGame(userChoice, computerChoice);
-        getScore();
-
-        displayUserScore.innerHTML = scorePlayer;
-        displayComputerScore.innerHTML = scoreIA;
-        displayUser.innerHTML = "<img src=imgs/"+userChoice+".png>";
-        displayIA.innerHTML = "<img src=imgs/"+computerChoice+".png>";
-
-        if (scorePlayer === 3 || scoreIA === 3) {
-            const restartImg = document.createElement("img");
-            restartImg.src = "imgs/repeat.png";
-            restartImg.alt = "Restart button";
-
-            const restartDiv = document.querySelector("#show-button");
-            const restartBtn = document.createElement("button")
-            restartBtn.classList = "restart-button";
-            restartBtn.appendChild(restartImg);
-            
-            // Get a function with the restart button append and set time out
-            function getRestartButton () {
-                restartDiv.appendChild(restartBtn);
-            }
-            setTimeout(getRestartButton, 2500);
-
-            // Creates a var with all 3 buttons length and a foor-loop to stop the count
-            const length = selectBtn.length;
-            for (let i = 0; i < length; i++) {
-                selectBtn[i].disabled = true;
-            }
-            //
-
-            // Get event foe the restart button
-            restartBtn.addEventListener("click", () => {
-                for (let i = 0; i < length; i++) {
-                    scoreIA = 0;
-                    scorePlayer = 0;
-                    displayComputerScore.innerHTML = "0";
-                    displayUserScore.innerHTML = "0";
-                    displayUser.innerHTML = "<img src=imgs/placeholder.png>";
-                    displayIA.innerHTML = "<img src=imgs/placeholder.png>";
-                    restartDiv.innerHTML = "";
-                    displayWinner.innerHTML = "Who will win this time?";
-                }
-
-                for (let i = 0; i < length; i++) {
-                    selectBtn[i].disabled = false;
-                }
-            })
+function resetButton() {
+    let resetDiv = document.querySelector('.reset');
+    let resetButton = document.createElement('button');
+    resetButton.className = 'btn btn-success';
+    resetButton.textContent = 'RESET GAME';
+    resetDiv.appendChild(resetButton);
+    resetButton.addEventListener('click', () => {
+        for (let i = 0; i < disableMessage; i++) {
+            buttons[i].disabled = false;
+            resetDiv.innerHTML = '';
+            userScore = '';
+            computerScore = ''
+            displayUserScore.innerHTML = storeUserMessage;
+            displayComputerScore.innerHTML = storeComputerMessage;
+            displayResultMessage.innerHTML = storeDefaultWinnerMessage;
         }
+    })
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let userChoice = button.dataset.selection;
+        let computerChoice = randomChoice();
+        userSelection.innerHTML = "<img src=imgs/"+userChoice+".png>";
+        computerSelection.innerHTML = "<img src=imgs/"+computerChoice+".png>";
+        setGame(userChoice, computerChoice);
+        winnerIs();
     })
 })
